1d2b245cbc60eb7ca0f9b8640d986e1010bf87e8
Wioletta Gluza <wiolagluza@gmail.com>
2025-07-28 15:08:41 +0200

    feat: basic Supabase client setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-const page = () => {
-  return <div>The Ghost Note App </div>;
-};
-export default page;
+import { createClient } from "@/utils/supabase/client";
+
+export default async function Instruments() {
+  const supabase = createClient();
+  const { data: instruments, error } = await supabase
+    .from("instruments")
+    .select();
+
+  if (error) return <p>Error loading instruments: {error.message}</p>;
+  if (!instruments) return <p>No instruments found.</p>;
+
+  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+}
