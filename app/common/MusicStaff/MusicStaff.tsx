1085da19e09b147d2a0a21dce88f57fb99a86a25
Wioletta Gluza <wiolagluza@gmail.com>
2025-07-30 14:41:32 +0200

    music staff scheletone

---
+++ app/common/MusicStaff/MusicStaff.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./MusicStaff.module.css";
+
+const MusicStaff = () => (
+  <div className={styles.staff}>
+    <div className={styles.line} />
+    <div className={styles.line} />
+    <div className={styles.line} />
+    <div className={styles.line} />
+    <div className={styles.line} />
+  </div>
+);
+
+export default MusicStaff;
